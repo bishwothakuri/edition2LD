6d35cef7c666888f8a58bc00010e742e9c46846c
bishwothakuri <mebishwo.shah@gmail.com>
2023-04-04 11:40:56 +0200

    Add a function to create a rdf graph and serialize it

---
+++ src/rdfa_transformer.py
@@ -0,0 +1,36 @@
+from typing import Dict
+
+from rdflib import Graph, Literal, Namespace, URIRef
+
+from serializers.rdfa_serializer import serialize_graph_to_rdfa
+
+FOAF_NS = Namespace("http://xmlns.com/foaf/0.1/")
+GN_NS = Namespace("http://www.geonames.org/ontology#")
+SKOS_NS = Namespace("http://www.w3.org/2004/02/skos/core#")
+DC_NS = Namespace("http://purl.org/dc/elements/1.1/")
+
+
+def create_rdf_graph(metadata: Dict) -> Graph:
+    persons = metadata.get("persons", [])
+    places = metadata.get("places", [])
+    terms = metadata.get("terms", [])
+
+    g = Graph()
+    for i, person in enumerate(persons):
+        person_uri = URIRef(f"http://example.org/person/{i}")
+        g.add((person_uri, FOAF_NS.name, Literal(person)))
+
+    for i, place in enumerate(places):
+        place_uri = URIRef(f"http://example.org/place/{i}")
+        g.add((place_uri, GN_NS.name, Literal(place)))
+
+    for i, term in enumerate(terms):
+        term_uri = URIRef(f"http://example.org/term/{i}")
+        g.add((term_uri, SKOS_NS.prefLabel, Literal(term)))
+
+    return g
+
+
+def convert_to_rdfa(metadata: Dict) -> str:
+    g = create_rdf_graph(metadata)
+    return serialize_graph_to_rdfa(g)
