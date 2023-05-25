28c10057f6ecab2e7a07ca499c21c14f97166146
Bishwo_Bijaya_Shah <bishwo-bijaya.shah@stud.uni-bamberg.de>
2023-05-26 00:56:03 +0200

    Fix the issue of resulting in redundant term meaning

--- conversion/xml_tei_converter.py
+++ conversion/xml_tei_converter.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
 
-
 def create_new_xml_tei(metadata):
     base_xml_file = "templates/base.xml"  # Path to the base XML-TEI template
 
@@ -9,9 +8,13 @@ def create_new_xml_tei(metadata):
     base_root = base_tree.getroot()
 
     # Find the relevant elements in the base XML
-    list_person_element = base_root.find(".//{http://www.tei-c.org/ns/1.0}listPerson")
-    list_place_element = base_root.find(".//{http://www.tei-c.org/ns/1.0}listPlace")
-    term_list_element = base_root.find(".//{http://www.tei-c.org/ns/1.0}termList")
+    ns = {"ns0": "http://www.tei-c.org/ns/1.0"}  # Namespace dictionary
+    list_person_element = base_root.find(".//ns0:listPerson", ns)
+    list_place_element = base_root.find(".//ns0:listPlace", ns)
+    term_list_element = base_root.find(".//ns0:termList", ns)
+
+    # Sort terms based on a key such as term or index
+    # sorted_terms = sorted(metadata["terms"], key=lambda x: x["term"])
 
     # Add the extracted metadata to the appropriate elements in the base XML
     for person in metadata["persons"]:
@@ -24,12 +27,14 @@ def create_new_xml_tei(metadata):
 
     for term in metadata["terms"]:
         term_element = ET.SubElement(term_list_element, "{http://www.tei-c.org/ns/1.0}term")
-        term_element.text = term["term"]
-        term_element.set("meaning", term["meaning"])
+        # term_element.text = term["term"]
+        # term_element.set("meaning", term["meaning"])
+
+        # Create orth and def elements inside term element
+        orth_element = ET.SubElement(term_element, "{http://www.tei-c.org/ns/1.0}orth")
+        orth_element.text = term["term"]
+        def_element = ET.SubElement(term_element, "{http://www.tei-c.org/ns/1.0}def")
+        def_element.text = term["meaning"]
 
     # Return the modified XML tree
     return ET.tostring(base_root, encoding="utf-8")
-
-
-    print("New XML-TEI file created successfully.")
-
