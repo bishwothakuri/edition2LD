abc656a309b3df22ce1a730f51976d804d6e5a7c
Bishwo_Bijaya_Shah <bishwo-bijaya.shah@stud.uni-bamberg.de>
2023-08-26 22:35:14 +0200

    Remove the angle bracket from the LOD identifiers

--- serialization/turtle.py
+++ serialization/turtle.py
@@ -22,7 +22,7 @@ def save_turtle_serialization(graph: Graph, file_path: str) -> None:
     turtle_data = graph.serialize(format='turtle')
 
     # Remove the angle brackets around subject, object, and predicate
-    # turtle_data = turtle_data.replace("<", "").replace(">", "")
+    turtle_data = turtle_data.replace("<", "").replace(">", "")
 
     # Replace full URIs with namespace prefixes
     for prefix, namespace in namespaces.items():
