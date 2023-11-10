2fa33226b15d138486510eda5092f0d03f4616e8
Bishwo_Bijaya_Shah <bishwo-bijaya.shah@stud.uni-bamberg.de>
2023-11-10 13:34:30 +0100

    Exclude the content of note and its descendant tags

--- metadata/xml_word_tokenizer.py
+++ metadata/xml_word_tokenizer.py
@@ -37,19 +37,14 @@ def tokenize_xml_text(xml_file_path):
         tokens = []
         id_counter = count(start=1)
 
-        for element in et_div.xpath('.//text() | .//foreign', namespaces=ns):
+        # Exclude content inside <note> tags and their descendants
+        for element in et_div.xpath('.//*[not(ancestor-or-self::tei:note)]/text()', namespaces=ns):
             if isinstance(element, str):
                 # If it's a string, tokenize its content
                 tokenizer = RegexpTokenizer(r'\w+|[^\w\s]')
                 text_tokens = tokenizer.tokenize(element)
                 valid_tokens = [token for token in text_tokens if valid_token_pattern.match(token)]
                 tokens.extend([(f"en_{next(id_counter):06}", token) for token in valid_tokens])
-            elif element.tag == '{http://www.tei-c.org/ns/1.0}foreign':
-                # If it's a foreign tag, extract and tokenize its text content
-                foreign_text = ' '.join([t.strip() for t in element.xpath('.//text()', namespaces=ns)])
-                tokenizer = RegexpTokenizer(r'\w+|[^\w\s]')
-                foreign_tokens = tokenizer.tokenize(foreign_text)
-                tokens.extend([(f"en_{next(id_counter):06}", token) for token in foreign_tokens])
 
         # Populate the tokens_dict with token IDs and their corresponding tokens
         for token_id, token in tokens:
